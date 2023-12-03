a4eb60d4dfe396996b45f486c0d01531ed8611a8
marsevilspirit <a2261214089@qq.com>
2023-12-03 17:39:01 +0800

    update

--- myls.c
+++ myls.c
@@ -1,9 +1,12 @@
 #include "myls.h"
 
-int main(int argc, char *argv[]) {
-    int i = 1;
-    int flag_use_arg = 0; // 标志是否有非命令行选项的命令行参数
+extern int optind, opterr, optopt;
+extern char * optarg;
+int flag_a = 0, flag_l = 0;
 
+int main(int argc, char *argv[]) {
+    int opt, i = 1;
+    int flag_use_arg = 0; // 标志是否有非命令行选项的命令行参数
     int j = 0;// 计算非选项参数的数量
     while (i < argc) {
         if (argv[i][0] != '-') {
@@ -21,7 +24,7 @@ int main(int argc, char *argv[]) {
     else {
         i = 1; // 重置 i 的值
         j = 0; // 重置 j 的值
-        // 遍历命令行参数
+               // 遍历命令行参数
         while (i < argc) 
         {
             if (argv[i][0] != '-') 
@@ -38,11 +41,45 @@ int main(int argc, char *argv[]) {
             use_arg[0] = ".";
         }
     }
-    
-for (int k = 0; k <= j; k++)
-{
-    judge_file(use_arg[k]);
-} 
+    while ((opt = getopt(argc, argv, "alRtris")) != -1)
+    {
+        switch (opt)
+        {
+            case 'a':      
+                flag_a = 1;
+                break;
+            case 'l':
+                flag_l = 1;
+                break;
+            case 'R':
+                printf("we use R\n");
+                break;
+            case 't':
+                printf("we use t\n");
+                break;
+            case 'r':
+                printf("we use r\n");
+                break;
+            case 'i':
+                printf("we use i\n");
+                break;
+            case 's':
+                printf("we use s\n");
+                break;
+            case '?':
+                printf("------------please don't do this-------------\n");
+                break;
+        }     
+    }
+
+    if(j == 0)
+        judge_file(use_arg[0]);
+
+    for (int k = 0; k < j; k++)
+    {
+        judge_file(use_arg[k]);
+        printf("\n");
+    } 
     // 释放动态分配的内存
     free(use_arg);
 
--- mylsfunc.c
+++ mylsfunc.c
@@ -1,20 +1,37 @@
 #include "myls.h"
-#include <stdio.h>
-#include <sys/stat.h>
+
+extern int flag_a;
 
 void judge_file(char * use_arg)//判断是文件还是目录
 {
     struct stat arg; 
     if(stat(use_arg, &arg) != 0)
         perror("stat");
-    
+
     switch (arg.st_mode & S_IFMT)
     {
         case S_IFDIR:
-            printf("this is a dir\n");
+            dir_list(use_arg); 
             break;
         default:
             printf("this is not a dir\n");
             break;
+    }
+}
+
+void dir_list(char * use_arg)
+{
+    DIR * dir; 
+    struct dirent * entry;
+
+    dir = opendir(use_arg);
+    if(dir == NULL)
+        perror("opendir");
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if(entry->d_name[0] == '.' && flag_a ==0)
+            continue;
+        printf("%s\n", entry->d_name);
     }
 }
