99164b4297d53c853e059bba811de29c16ed55be
marsevilspirit <a2261214089@qq.com>
2023-12-12 20:17:20 +0800

    update

---
+++ t_system.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#define MAX_CMD_LEN 200
+
+int main(void)
+{
+    char str[MAX_CMD_LEN];
+    int status;
+
+    for(;;)
+    {
+        printf("mar:$");
+        fflush(stdout);
+        if(fgets(str, MAX_CMD_LEN, stdin) == NULL)
+            break;
+
+        status = system(str);
+        printf("systen() returned: status=0x%04x (%d,%d)\n", (unsigned int)status, status >> 8, status & 0xff);
+
+        if(status == -1)
+        {
+            perror("status");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            if(WIFEXITED(status) && WEXITSTATUS(status) == 127)
+                printf("(Probably) could not invoke shell\n");
+        }
+    } 
+    exit(EXIT_SUCCESS);
+}
